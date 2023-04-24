73fe981fe2cb3b9a89d890296987147cfa7b6ee0
JohannVargas <johannvargas767@u.boisestate.edu>
2023-04-24 13:39:03 -0600

    PDO credentials

--- src/php/pdo-connection.php
+++ src/php/pdo-connection.php
@@ -1,8 +1,8 @@
 <?php
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "findmeabreak";
+$dbServername = "us-cdbr-east-06.cleardb.net";
+$dbUsername = "bac890cd1d657c";
+$dbPassword = "3f3da4cc";
+$dbName = "heroku_4050c3bcd543143";
 
 $options = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
