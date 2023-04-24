b8a4edc698e1e3ae988f53b1f0f119b754274842
JohannVargas <johannvargas767@u.boisestate.edu>
2023-04-24 00:56:47 -0600

    sanitazing data

--- src/php/college-reviews.php
+++ src/php/college-reviews.php
@@ -1,35 +1,53 @@
-<!DOCTYPE html>
 <?php
-include_once('dbh.inc.php');
+include_once 'dbh.inc.php';
 session_start();
-$username = '';
-if (isset($_SESSION['username']) && isset($_SESSION['email'])) {
-    $username = htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8');
+
+// Ensure user is logged in
+if (!isset($_SESSION['username']) || !isset($_SESSION['email'])) {
+  header('Location: login.php');
+  exit();
 }
 
-$collegeID = htmlspecialchars($_GET['id'], ENT_QUOTES, 'UTF-8');
+$username = $_SESSION['username'];
+$collegeID = mysqli_real_escape_string($conn, $_GET['id']);
 
-$sql = "SELECT name FROM collegelist WHERE ID = ?";
-$stmt = mysqli_prepare($conn, $sql);
-mysqli_stmt_bind_param($stmt, "s", $collegeID);
-mysqli_stmt_execute($stmt);
-$result = mysqli_stmt_get_result($stmt);
+// Sanitize college ID input
+if (!ctype_digit($collegeID)) {
+  header('Location: error.php');
+  exit();
+}
+
+// Retrieve college name
+$sql = "SELECT name FROM collegelist WHERE ID = '$collegeID'";
+$result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
 $collegeName = htmlspecialchars($row['name'], ENT_QUOTES, 'UTF-8');
 
-if(isset($_POST['review_text']) && isset($_POST['rating'])) {
-    $review_text = htmlspecialchars($_POST['review_text'], ENT_QUOTES, 'UTF-8');
-    $rating = htmlspecialchars($_POST['rating'], ENT_QUOTES, 'UTF-8');
+// Handle form submission
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $review_text = mysqli_real_escape_string($conn, $_POST['review_text']);
+  $rating = (int) $_POST['rating'];
+
+  // Sanitize review and rating inputs
+  $review_text = htmlspecialchars($review_text, ENT_QUOTES, 'UTF-8');
+  $rating = filter_var($rating, FILTER_VALIDATE_INT, array('options' => array('min_range' => 1, 'max_range' => 5)));
+
+  // Validate input
+  if (empty($review_text) || $rating === false) {
+    $error = 'Please enter a valid review and rating.';
+  } else {
     $user_id = $_SESSION['id'];
     $date = date('Y-m-d');
-    $sql = "INSERT INTO college_reviews (college_id, user_id, review_text, rating, review_date)
-            VALUES (?, ?, ?, ?, ?)";
-    $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "sssss", $collegeID, $user_id, $review_text, $rating, $date);
-    mysqli_stmt_execute($stmt);
 
+    // Insert review into database
+    $sql = "INSERT INTO college_reviews (college_id, user_id, review_text, rating, review_date)
+            VALUES ('$collegeID', '$user_id', '$review_text', '$rating', '$date')";
+    mysqli_query($conn, $sql);
+
+    // Redirect to same page
     header('Location: '.$_SERVER['PHP_SELF'].'?id='.$collegeID);
     exit();
+  }
 }
 ?>
 <!DOCTYPE html>
@@ -49,21 +67,22 @@ if(isset($_POST['review_text']) && isset($_POST['rating'])) {
       <h1 class="heading"><?php echo htmlspecialchars($collegeName); ?> Reviews</h1>
       <ul class="reviews-list">
         <?php
+        $collegeID_sanitized = mysqli_real_escape_string($conn, $collegeID);
         $sql = "SELECT college_reviews.*, users.username
                 FROM college_reviews
                 JOIN users ON college_reviews.user_id = users.id
-                WHERE college_reviews.college_id = '$collegeID'";
+                WHERE college_reviews.college_id = '$collegeID_sanitized'";
         $result = mysqli_query($conn, $sql);
         if (mysqli_num_rows($result) == 0) {
           echo "<p>No reviews yet. Be the first one to write a review!</p>";
         }
         else{
         while ($row = mysqli_fetch_assoc($result)) {
-           echo "<li class='review'>";
-          echo "<h3 class='username'>" . htmlspecialchars($row['username'], ENT_QUOTES) . "</h3>";
-          echo "<p class='review-text'>" . htmlspecialchars($row['review_text'], ENT_QUOTES) . "</p>";
-          echo "<div class='rating'>" . htmlspecialchars($row['rating'], ENT_QUOTES) . "</div>";
-          echo "<div class='review-date'>" . htmlspecialchars($row['review_date'], ENT_QUOTES) . "</div>";
+          echo "<li class='review'>";
+          echo "<h3 class='username'>" . htmlspecialchars($row['username']) . "</h3>";
+          echo "<p class='review-text'>" . htmlspecialchars($row['review_text']) . "</p>";
+          echo "<div class='rating'>" . htmlspecialchars($row['rating']) . "</div>";
+          echo "<div class='review-date'>" . htmlspecialchars($row['review_date']) . "</div>";
           echo "</li>";
         }
       }
@@ -71,15 +90,15 @@ if(isset($_POST['review_text']) && isset($_POST['rating'])) {
       </ul>
       <?php if (!empty($username)): ?>
       <h2 class="heading">Submit a Review</h2>
-      <form class="review-form" action="<?php echo $_SERVER['PHP_SELF'].'?id='.$collegeID; ?>" method="POST">
+      <form class="review-form" action="" method="POST">
         <input type="hidden" name="college_id" value="<?php echo htmlspecialchars($collegeID); ?>">
         <div class="form-group">
           <label for="review_text">Review:</label>
-          <textarea name="review_text" id="review_text" rows="5" cols="30" class="form-control"></textarea>
+          <textarea name="review_text" id="review_text" rows="5" cols="30" class="form-control"><?php if(isset($_POST['review_text'])) echo htmlspecialchars($_POST['review_text']); ?></textarea>
         </div>
         <div class="form-group">
           <label for="rating">Rating:</label>
-          <input type="number" name="rating" id="rating" min="1" max="5" class="form-control">
+          <input type="number" name="rating" id="rating" min="1" max="5" class="form-control" value="<?php if(isset($_POST['rating'])) echo htmlspecialchars($_POST['rating']); ?>">
         </div>
         <div class="form-group">
           <button type="submit" class="btn btn-primary">Submit</button>
--- src/php/destination-review.php
+++ src/php/destination-review.php
@@ -8,15 +8,17 @@ if (isset($_SESSION['username']) && isset($_SESSION['email'])) {
     $username = $_SESSION['username'];
 }
 
-$destinationID = $_GET['id'];
+$destinationID = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
 $sql = "SELECT destinations.destinationname, states.statename
 FROM destinations
 JOIN states ON destinations.stateID = states.stateID
-WHERE destinations.destinationID = '$destinationID'
-";
+WHERE destinations.destinationID = ?";
 
-$result = mysqli_query($conn, $sql);
+$stmt = mysqli_prepare($conn, $sql);
+mysqli_stmt_bind_param($stmt, "i", $destinationID);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
 $row = mysqli_fetch_assoc($result);
 $destinationname = $row['destinationname'];
 $statename = $row['statename'];
@@ -24,21 +26,26 @@ $statename = $row['statename'];
 $sql = "SELECT destination_reviews.*, users.username
 FROM destination_reviews
 JOIN users ON destination_reviews.user_id = users.id
-WHERE destination_reviews.destination_id = '$destinationID'
-ORDER BY destination_reviews.review_date DESC
-";
+WHERE destination_reviews.destination_id = ?
+ORDER BY destination_reviews.review_date DESC";
 
-$result = mysqli_query($conn, $sql);
+$stmt = mysqli_prepare($conn, $sql);
+mysqli_stmt_bind_param($stmt, "i", $destinationID);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+
 if(isset($_POST['review_text']) && isset($_POST['rating'])) {
-    $review_text = mysqli_real_escape_string($conn, $_POST['review_text']);
-    $rating = mysqli_real_escape_string($conn, $_POST['rating']);
+    $review_text = filter_input(INPUT_POST, 'review_text', FILTER_SANITIZE_STRING);
+    $rating = filter_input(INPUT_POST, 'rating', FILTER_SANITIZE_NUMBER_INT);
     $user_id = $_SESSION['id'];
     $date = date('Y-m-d');
     $sql = "INSERT INTO destination_reviews (destination_id, user_id, review_text, rating, review_date)
-            VALUES ('$destinationID', '$user_id', '$review_text', '$rating', '$date')";
-    mysqli_query($conn, $sql);
+            VALUES (?, ?, ?, ?, ?)";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "iisis", $destinationID, $user_id, $review_text, $rating, $date);
+    mysqli_stmt_execute($stmt);
 
-    header('Location: '.$_SERVER['PHP_SELF'].'?id='.$collegeID);
+    header('Location: '.$_SERVER['PHP_SELF'].'?id='.$destinationID);
     exit();
 }
 ?>
@@ -60,8 +67,8 @@ if(isset($_POST['review_text']) && isset($_POST['rating'])) {
   ?>
   <div class="Banner"></div>
   <div class="container">
-    <h1 class="heading"><?php echo $destinationname; ?> Reviews</h1>
-    <h2 class="heading"><?php echo $statename; ?></p>
+    <h1 class="heading"><?php echo htmlspecialchars($destinationname, ENT_QUOTES, 'UTF-8'); ?> Reviews</h1>
+    <h2 class="heading"><?php echo htmlspecialchars($statename, ENT_QUOTES, 'UTF-8'); ?></h2>
     <ul class="reviews-list">
     <?php 
     if (mysqli_num_rows($result) == 0) {
@@ -69,10 +76,10 @@ if(isset($_POST['review_text']) && isset($_POST['rating'])) {
     } else {
       while ($row = mysqli_fetch_assoc($result)) {
         echo "<li class='review'>";
-        echo "<h3 class='username'>" . $row['username'] . "</h3>";
-        echo "<p class='review-text'>" . $row['review_text'] . "</p>";
-        echo "<div class='rating'>" . $row['rating'] . "</div>";
-        echo "<div class='review-date'>" . $row['review_date'] . "</div>";
+        echo "<h3 class='username'>" . htmlspecialchars($row['username'], ENT_QUOTES, 'UTF-8') . "</h3>";
+        echo "<p class='review-text'>" . htmlspecialchars($row['review_text'], ENT_QUOTES, 'UTF-8') . "</p>";
+        echo "<div class='rating'>" . htmlspecialchars($row['rating'], ENT_QUOTES, 'UTF-8') . "</div>";
+        echo "<div class='review-date'>" . htmlspecialchars($row['review_date'], ENT_QUOTES, 'UTF-8') . "</div>";
         echo "</li>";
       }
     }
@@ -81,14 +88,14 @@ if(isset($_POST['review_text']) && isset($_POST['rating'])) {
       <?php if (!empty($username)): ?>
       <h2 class="heading">Submit a Review</h2>
       <form class="review-form" action="" method="POST">
-        <input type="hidden" name="college_id" value="<?php echo $destinationID; ?>">
+        <input type="hidden" name="college_id" value="<?php echo htmlspecialchars($destinationID, ENT_QUOTES, 'UTF-8'); ?>">
         <div class="form-group">
           <label for="review_text">Review:</label>
-          <textarea name="review_text" id="review_text" rows="5" cols="30" class="form-control"></textarea>
+          <textarea name="review_text" id="review_text" rows="5" cols="30" class="form-control"><?php echo isset($_POST['review_text']) ? htmlspecialchars($_POST['review_text'], ENT_QUOTES, 'UTF-8') : ''; ?></textarea>
         </div>
         <div class="form-group">
           <label for="rating">Rating:</label>
-          <input type="number" name="rating" id="rating" min="1" max="5" class="form-control">
+          <input type="number" name="rating" id="rating" min="1" max="5" class="form-control" value="<?php echo isset($_POST['rating']) ? htmlspecialchars($_POST['rating'], ENT_QUOTES, 'UTF-8') : ''; ?>">
         </div>
         <div class="form-group">
           <button type="submit" class="btn btn-primary">Submit</button>
